9a803a2c612357c9ec4caf75f52b6344b3a67522
farazahmed6 <132001664+farazahmed6@users.noreply.github.com>
2023-09-06 04:50:26 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+  {
+    type: "number",
+    name: "numberOne",
+    message: "Enter the First number: ",
+  },
+  {
+    type: "number",
+    name: "numberTwo",
+    message: "Enter the Second number: ",
+  },
+  {
+    type: "list",
+    name: "operator",
+    message: "select the operator you want to perform: ",
+    choices: ["Add", "Subtract", "Multiply", "Divide"],
+  },
+]);
+
+let result: number;
+
+switch (answers.operator) {
+  case "Add":
+    result = answers.numberOne + answers.numberTwo;
+    console.log(" The answer for the Addition is : " + result);
+    break;
+
+  case "Subtract":
+    result = answers.numberOne - answers.numberTwo;
+    console.log(" The answer for the Subtraction is : " + result);
+    break;
+
+  case "Multiply":
+    result = answers.numberOne * answers.numberTwo;
+    console.log(" The answer for the Multiplication is: " + result);
+    break;
+
+  case " Divide":
+    result = answers.numberOne / answers.numberTwo;
+    console.log(" The answer for the Division is:  " + result);
+    break;
+}
